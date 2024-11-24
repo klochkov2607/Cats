f937ce5a459513daf08c9aabdb62820981f10a55
klochkov2607 <klochkov2607@gmail.com>
2024-11-24 18:39:32 +0300

    Создано окно и виджеты

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,26 @@
 from tkinter import*
+from PIL import Image, ImageTk#PIL, для работы с изображениями
+import requests#для получения информации из интернета
+from io import BytesIO#библиотека io import позволяет работать с вводом/выводом
+#информации, а BytesIO с двойничными единичками, ноликами, байтами
+
+
+window.Tk()
+window.title(text="Cats!")
+window.geometry("600x480")
+label=Label()
+label.pack()
+
+url="https://cataas.com/cat"
+img=load_image(url)
+
+
+if img:
+    label.config(image=img)
+    label.image=img#чтобы "сборщик мусора" не посчитал картинку таковым
+
+window.mainloop()
+
+
+
+
